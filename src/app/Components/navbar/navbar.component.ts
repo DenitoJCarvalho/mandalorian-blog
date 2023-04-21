e5f3a71e6d83758b1b5b2c6747fe797dc2237d56
Dennis <dennisjcarvalho@gmail.com>
2023-04-21 11:54:16 -0300

    fix: corrigido menu desktop

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  @ViewChild('menuList') menuList!: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  showMenuMobile() {
+    if (window.screen.width < 1024) {
+      if (this.menuList.nativeElement.style.display === 'none' || this.menuList.nativeElement.style.display == '') {
+        this.menuList.nativeElement.style.display = 'flex';
+      } else {
+        this.menuList.nativeElement.style.display = 'none';
+      }
+    }
+
+    if (window.screen.width >= 1024) {
+      this.menuList.nativeElement.style.display = 'flex';
+    }
+
+  }
+
+  verifySizeScreen() {
+    console.log(window.screen.width);
+  }
 
 }
